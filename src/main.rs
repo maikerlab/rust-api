e213340bb737afbc2fc92f8c6362f8e85146ae73
Maik Lorenz <lorenz.maik@web.de>
2023-01-23 14:25:45 +0000

    Add handlers for path and query variables

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,46 @@
-use warp::Filter;
+use std::{collections::HashMap, fmt::format};
+use warp::{http::StatusCode, Filter};
+
+async fn hello_world() -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Hello, World!"))
+}
+
+async fn hello(param: String) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Hello {}!", param))
+}
+
+async fn hello_query(param: HashMap<String, String>) -> Result<impl warp::Reply, warp::Rejection> {
+    let name = param.get("name");
+    match name {
+        Some(name) => Ok(format!("Hello, {}!", name)),
+        None => Ok(format!("Hello, World! (Name is unknown)")),
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    // GET /hello => 200 OK with body "Hello, World!"
+    let hello_world = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::path::end())
+        .and_then(hello_world);
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and_then(hello);
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello_query = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::query::<HashMap<String, String>>())
+        .and(warp::path::end())
+        .and_then(hello_query);
+
+    // Serve routes
+    let routes = hello_query.or(hello_world).or(hello);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
